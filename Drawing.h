d4635962126626962f011eb65a8dbe83296cccf7
Kvant85 <Kvant85@mail.ru>
2024-04-01 12:26:02 +0300

    Delete Drawing.h

--- Drawing.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void drawing(sf::RenderWindow* window, vector<sf::Texture>* texture, Map* gameMap, Player* player,
-				vector<Enemy*>* enemy, sf::Clock* animationTimer, int gameState, Caption* score);
